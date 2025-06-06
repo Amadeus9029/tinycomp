367c271150a1c353e7613f14320d456d99bd6053
Amadeus9029 <965720890@qq.com>
2025-06-06 15:01:13 +0800

    ref：版本修改

--- tinycomp/__init__.py
+++ tinycomp/__init__.py
@@ -4,7 +4,7 @@ TinyComp - A Python package for compressing images using TinyPNG API
 
 from .compressor import TinyCompressor
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Amadeus9029"
 __email__ = "965720890@qq.com"
 
