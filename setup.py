92f944d0f0e5ad0c3aaf0fe772d53375291394dd
Amadeus9029 <965720890@qq.com>
2025-06-09 11:42:09 +0800

    ref：更新为0.1.2版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="tinycomp-amadeus",
-    version="0.1.1",
+    version="0.1.2",
     author="Amadeus9029",
     author_email="965720890@qq.com",
     description="A Python package for compressing images using TinyPNG API",
